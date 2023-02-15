09aa6ce1a7c79fc757e98af0907136e0a3989d53
Svyatoslav <85786374+slivanovich@users.noreply.github.com>
2023-02-15 18:24:27 +0300

    Add files via upload

---
+++ src/def.hpp
@@ -0,0 +1,23 @@
+#ifndef GLUT_TEST_DEF_H
+#define GLUT_TEST_DEF_H
+
+// Include realization of using libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// All useful includes
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <cassert>
+
+// Typedefs (only for our convenience)
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+#endif // GLUT_TEST_DEF_H

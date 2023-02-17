f9751ef11c1a1fb22696c411dc7c6903be6e3a7a
ttcpip <38619302+ttcpip@users.noreply.github.com>
2023-02-17 18:26:41 +0300

    refactor: using clang-format

--- src/def.hpp
+++ src/def.hpp
@@ -1,24 +1,24 @@
-#ifndef DEF_H
-#define DEF_H
+#ifndef DEF_HPP
+#define DEF_HPP
 
 // Include header files of using libraries
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
 
 // All useful includes
+#include <algorithm>
+#include <cassert>
+#include <cmath>
 #include <iostream>
-#include <vector>
 #include <map>
+#include <memory>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
-#include <cmath>
-#include <algorithm>
-#include <memory>
-#include <cassert>
+#include <vector>
 
 // Typedefs (only for our convenience)
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
-#endif // DEF_H
+#endif  // DEF_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,66 @@
-#include "def.hpp" // Should be in all *.hpp files (for now it's right information)
+#include "def.hpp"  // Should be in all *.hpp files (for now it's right information)
 
 // Project namespace
 namespace cgsg {
-    // Render class declaration
-    class render {
-    public:
-        // Default constructor
-        explicit render() noexcept = default;
-    }; // End of 'render' class
+// Render class declaration
+class render {
+public:
+    // Default constructor
+    explicit render() noexcept = default;
+};  // End of 'render' class
 
-    // Shader class declaration
-    class shader {
-    private:
-        GLuint shader_id; // shader id
+// Shader class declaration
+class shader {
+private:
+    GLuint shader_id;  // shader id
 
-        // Default constructor
-        explicit shader() noexcept : shader_id(0) {
-        } // End of 'shader' function
-    }; // End of 'shader' class
+    // Default constructor
+    explicit shader() noexcept : shader_id(0) {
+    }  // End of 'shader' function
+};     // End of 'shader' class
 
-    // Vertex buffer class declaration
-    class vertex_buffer {
-    private:
-        // Default constructor
-        explicit vertex_buffer() noexcept = default;
-    }; // End of 'vertex_buffer' class
+// Vertex buffer class declaration
+class vertex_buffer {
+private:
+    // Default constructor
+    explicit vertex_buffer() noexcept = default;
+};  // End of 'vertex_buffer' class
 
-    // Primitive class declaration
-    class primitive {
-    private:
-        // Default constructor
-        explicit primitive() noexcept = default;
-    }; // End of 'primitive' class
+// Primitive class declaration
+class primitive {
+private:
+    // Default constructor
+    explicit primitive() noexcept = default;
+};  // End of 'primitive' class
 
-    // Manager class declaration
-    class manager : public render {
-    private:
-        ::std::vector<::std::unique_ptr<primitive>> primiteves; // Storage of all primitives
-        ::std::vector<::std::unique_ptr<shader>> shaders;       // Storage of all shaders
+// Manager class declaration
+class manager : public render {
+private:
+    ::std::vector<::std::unique_ptr<primitive>> primiteves;  // Storage of all
+                                                             // primitives
+    ::std::vector<::std::unique_ptr<shader>> shaders;  // Storage of all shaders
 
-        // Default constructor
-        explicit manager() noexcept : render() {
-        } // End of 'manager' function
+    // Default constructor
+    explicit manager() noexcept : render() {
+    }  // End of 'manager' function
 
-    public:
-        /* Get instance of manager class function.
-         * ARGUMENTS: None.
-         * RETURNS:
-         *   (manager &) - instance of manager class
-         */
-        [[nodiscard]] static manager & get_manager() {
-            static manager singleton;
-            return singleton;
-        } // End of 'get_manager' function
-    }; // End of 'manager' class
-} // End of 'cgsg' namespace
+public:
+    /* Get instance of manager class function.
+     * ARGUMENTS: None.
+     * RETURNS:
+     *   (manager &) - instance of manager class
+     */
+    [[nodiscard]] static manager &get_manager() {
+        static manager singleton;
+        return singleton;
+    }  // End of 'get_manager' function
+};     // End of 'manager' class
+}  // namespace cgsg
 
 // Main program function
 int main() {
     // glfw initialization
-    if (!glfwInit())
-        assert("Error in glfw initialization");
+    if (!glfwInit()) assert("Error in glfw initialization");
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -69,7 +69,7 @@ int main() {
 #endif
 
     // Create our window (pointer)
-    GLFWwindow * window = glfwCreateWindow(400, 400, "SK4", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(400, 400, "SK4", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         assert("Error in glfw window creation");
@@ -78,12 +78,12 @@ int main() {
     glfwMakeContextCurrent(window);
 
     // Initialization of OpenGL
-    if (!glewInit())
-        assert("Error in glew initialization");
+    if (!glewInit()) assert("Error in glew initialization");
 
     // Debug information (OpenGL/glsl version)
     ::std::cout << "OpenGL: " << glGetString(GL_VERSION) << "\n";
-    ::std::cout << "Shader language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << ::std::endl;
+    ::std::cout << "Shader language: "
+                << glGetString(GL_SHADING_LANGUAGE_VERSION) << ::std::endl;
 
     // Run window response infinity cycle (can be used in threads)
     while (!glfwWindowShouldClose(window)) {
@@ -97,4 +97,4 @@ int main() {
     glfwTerminate();
 
     return 0;
-} // End of 'main' function
+}  // End of 'main' function

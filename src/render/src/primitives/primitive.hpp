8ba06574723f550dd767ed1a72db477ad64ff588
Svyatoslav <85786374+slivanovich@users.noreply.github.com>
2023-03-28 21:31:24 +0300

    Add files via upload

---
+++ src/render/src/primitives/primitive.hpp
@@ -0,0 +1,172 @@
+#ifndef PRIMITIVE_HPP
+#define PRIMITIVE_HPP
+
+#include "../../../def.hpp"
+#include "../../../utilities/cameras/camera.hpp"
+#include "../buffers/buffer.hpp"
+#include "../shaders/shader.hpp"
+
+namespace hse {
+// Primitive class declaration
+class primitive {
+    // Friend classes
+    friend class unit;
+
+    buffer::renderType renderingType;  // Rendering type
+    vertexArray *vertexArrayInstance;  // Vertex array instance
+    uint shaderProgramId;              // Primitive's shader instance
+    bool isVisible;                    // Visibility flag
+    math::matr4 transform;             // Transform matrix
+
+    ::std::map<const char *, int *> shaderUniform1i;    // Shader's int uniforms
+    ::std::map<const char *, float *> shaderUniform1f;  // Shader's float
+                                                        // uniforms
+    ::std::map<const char *, float> shaderUniform3fv;   // Shader's float
+                                                        // 3-component vector
+                                                        // uniforms
+    ::std::map<const char *, ::std::pair<float *, int>>
+        shaderUniform4fv;  // Shader's float 3-component vector uniforms
+    // + count of these vectors
+
+    // Class default constructor
+    explicit primitive();
+
+    /* Class constructor.
+     * ARGUMENTS:
+     *   - primitive's shader program id:
+     *       uint primitiveShaderProgramId_;
+     *   - vertex buffer data:
+     *       const ::std::vector<float> &vertexBuffer;
+     *   - vertex buffer format:
+     *       const ::std::string &vertexBufferFormat;
+     *   - index buffer data:
+     *       const ::std::vector<int> &indexBuffer;
+     *   - primitive's rendering type:
+     *       renderType type_;
+     * NOTE: vertexBufferFormat - use default type or "v3v3v3v2" == vertex
+     * position, color, normal, texture coordinate.
+     */
+    explicit primitive(
+        uint primitiveShaderProgramId_,
+        const ::std::vector<float> &vertexBuffer,
+        const ::std::string &vertexBufferFormat = "v3v3",
+        const ::std::vector<int> &indexBuffer = ::std::vector<int>(),
+        buffer::renderType type_ = buffer::renderType::TRIANGLES
+    );
+
+    /* Class constructor.
+     * ARGUMENTS:
+     *   - primitive's shader program id:
+     *       uint primitiveShaderProgramId_;
+     *   - vertex array:
+     *       vertexArray *vertexArrayInstance_;
+     *   - primitive's rendering type:
+     *       renderType type_.
+     */
+    explicit primitive(
+        uint primitiveShaderProgramId_,
+        vertexArray *vertexArrayInstance_,
+        buffer::renderType type_ = buffer::renderType::TRIANGLES
+    );
+
+    /* Draw primitive function.
+     * ARGUMENTS:
+     *   - camera for rendering primitive:
+     *      const camera &camera;
+     * RETURNS: None.
+     */
+    void render(const camera &camera) const;
+
+    // Class destructor
+    ~primitive();
+
+public:
+    /* Attach shader program id to the primitive function.
+     * ARGUMENTS:
+     *   - shader program id:
+     *       uint shaderProgramId_;
+     * RETURNS: None.
+     */
+    void setShaderProgram(uint shaderProgramId_);
+
+    /* Get primitive rendering type function.
+     * ARGUMENTS: None.
+     * RETURNS:
+     *   (buffer::renderType) - rendering type.
+     */
+    buffer::renderType getRenderType() const;
+
+    /* Set primitive rendering type function.
+     * ARGUMENTS:
+     *   - new rendering type:
+     *       buffer::renderType type_:
+     * RETURNS: None.
+     */
+    void setRenderType(buffer::renderType type_);
+
+    /* Get primitive's visibility flag function.
+     * ARGUMENTS: None.
+     * RETURNS:
+     *   (bool) - visibility flag.
+     */
+    bool getVisibility() const;
+
+    /* Set primitive's visibility flag function.
+     * ARGUMENTS:
+     *   - new flag:
+     *       bool isVisible_;
+     * RETURNS: None.
+     */
+    void setVisibility(bool isVisible_);
+
+    /* Add uniform to the shader function.
+     * ARGUMENTS:
+     *   - uniform value:
+     *       const int *uniformValue;
+     *   - uniform name on the shader:
+     *       const char *uniformName;
+     * RETURNS: None.
+     */
+    void addUniform(const int *uniformValue, const char *uniformName);
+
+    /* Add uniform to the shader function.
+     * ARGUMENTS:
+     *   - uniform value:
+     *       const float *uniformValue;
+     *   - uniform name on the shader:
+     *       const char *uniformName;
+     * RETURNS: None.
+     */
+    void addUniform(const float *uniformValue, const char *uniformName);
+
+    /* Add uniform to the shader function.
+     * ARGUMENTS:
+     *   - uniform value:
+     *       const math::vec3 &vector;
+     *   - uniform name on the shader:
+     *       const char *uniformName;
+     *   - uniforms number:
+     *       int uniformCount;
+     * RETURNS: None.
+     */
+    void addUniform(const math::vec3 &vector, const char *uniformName);
+
+    /* Add uniform to the shader function.
+     * ARGUMENTS:
+     *   - uniform value:
+     *       const float *uniformValue;
+     *   - uniform name on the shader:
+     *       const char *uniformName;
+     *   - uniforms number:
+     *       int uniformCount;
+     * RETURNS: None.
+     */
+    void addUniform(
+        const float *uniformValue,
+        const char *uniformName,
+        int uniformCount
+    );
+};  // End of 'primitive' class
+}  // namespace hse
+
+#endif  // PRIMITIVE_HPP
---
+++ src/render/src/units/unit.hpp
@@ -0,0 +1,215 @@
+#ifndef UNIT_HPP
+#define UNIT_HPP
+
+#include "../../../def.hpp"
+#include "../../../utilities/cameras/camera.hpp"
+#include "../buffers/buffer.hpp"
+#include "../primitives/primitive.hpp"
+#include "../shaders/shader.hpp"
+
+namespace hse {
+// Unit class declaration
+class unit {
+    // Friend classes
+    friend class render;
+
+    bool isVisible;      // Unit's visibility flag
+    bool isInitialized;  // We can have a lot of copies of one unit,
+                         // but we want to initialize it only once
+    ::std::map<::std::string, shader *> shadersArray;  // Unit's shader programs
+                                                       // array
+    ::std::vector<primitive *> primitivesArray;  // Unit's primitives array
+    ::std::vector<buffer *> buffersArray;        // Unit's buffers array
+
+protected:
+    camera mainCamera;  // Unit's main camera
+
+public:
+    /* Get unit's visibility flag function.
+     * ARGUMENTS: None.
+     * RETURNS:
+     *   (bool) - visibility flag.
+     */
+    bool getVisibility() const;
+
+    /* Set unit's visibility flag function.
+     * ARGUMENTS:
+     *   - new flag:
+     *       bool isVisible_;
+     * RETURNS: None.
+     */
+    void setVisibility(bool isVisible_);
+
+    /* Create vertex buffer function.
+     * ARGUMENTS:
+     *   - buffer's data:
+     *       const ::std::vector<float> &vertexBufferData;
+     *   - buffer's format:
+     *       const ::std::string &vertexBufferFormat;
+     * NOTE: vertexBufferFormat - use default type or "v3v3v3v2" == vertex
+     * position, color, normal, texture coordinate.
+     */
+    uint createVertexBuffer(
+        const ::std::vector<float> &vertexBufferData,
+        const ::std::string &vertexBufferFormat = "v3"
+    );
+
+    /* Create index buffer function.
+     * ARGUMENTS:
+     *   - buffer's data;
+     *       const ::std::vector<int> &indexBufferData.
+     */
+    uint createIndexBuffer(const ::std::vector<int> &indexBufferData);
+
+    /* Create vertex array function.
+     * ARGUMENTS:
+     *   - vertex buffer data:
+     *       const ::std::vector<float> &vertexBufferData;
+     *   - vertex buffer format:
+     *       const ::std::string &vertexBufferFormat;
+     *   - index buffer data:
+     *       const ::std::vector<int> &indexBufferData;
+     * NOTE: vertexBufferFormat - use default type or "v3v3v3v2" == vertex
+     * position, color, normal, texture coordinate.
+     */
+    uint createVertexArray(
+        const ::std::vector<float> &vertexBufferData,
+        const ::std::string &vertexBufferFormat,
+        const ::std::vector<int> &indexBufferData
+    );
+
+    /* Create shader storage buffer function.
+     * ARGUMENTS:
+     *   - buffer's data:
+     *       const ::std::vector<T> &bufferData;
+     *   - buffer's binding value:
+     *       uint bufferBinding.
+     */
+    template <typename T>
+    uint createShaderStorageBuffer(
+        const ::std::vector<T> &bufferData,
+        uint bufferBinding
+    );
+
+    /* Create shader function.
+     * ARGUMENTS:
+     *   - path to shader's realization (read shader class constructor note)
+     *       const ::std::string &shaderPath;
+     * RETURNS:
+     *   (uint) - shader program id.
+     */
+    uint createShader(const ::std::string &shaderPath);
+
+    /* Create shader function.
+     * ARGUMENTS:
+     *   - vertex shader source in string:
+     *       const ::std::string &vertexShaderSource;
+     *   - fragment shader source in string:
+     *       const ::std::string &fragmentShaderSource;
+     *   - path to shader's realization (read shader class constructor note)
+     *       const ::std::string &shaderPath;
+     * RETURNS:
+     *   (uint) - shader program id.
+     */
+    uint createShader(
+        const ::std::string &vertexShaderSource,
+        const ::std::string &fragmentShaderSource,
+        const ::std::string &shaderPath = ""
+    );
+
+    /* Create primitive function.
+     * ARGUMENTS:
+     *   - path to primitive's shader:
+     *       const ::std::string &shaderPath;
+     *   - vertex buffer data:
+     *       const ::std::vector<float> &vertexBufferData;
+     *   - vertex buffer format:
+     *       const ::std::string &vertexBufferFormat;
+     *   - index buffer data:
+     *       const ::std::vector<int> &indexBufferData;
+     * RETURNS:
+     *   (primitive *) - not-owning pointer to the created primitive;
+     * NOTE: vertexBufferFormat - use default type or "v3v3v3v2" == vertex
+     * position, color, normal, texture coordinate.
+     */
+    primitive *createPrimitive(
+        const ::std::string &shaderPath,
+        const ::std::vector<float> &vertexBufferData,
+        const ::std::string &vertexBufferFormat = "v3v3",
+        const ::std::vector<int> &indexBufferData = ::std::vector<int>()
+    );
+
+    /* Create primitive function.
+     * ARGUMENTS:
+     *   - shader program id:
+     *       uint shaderProgramId;
+     *   - vertex buffer data:
+     *       const ::std::vector<float> &vertexBufferData;
+     *   - vertex buffer format:
+     *       const ::std::string &vertexBufferFormat;
+     *   - index buffer data:
+     *       const ::std::vector<int> &indexBufferData;
+     * RETURNS:
+     *   (primitive *) - not-owning pointer to the created primitive;
+     */
+    primitive *createPrimitive(
+        uint shaderProgramId,
+        const ::std::vector<float> &vertexBufferData,
+        const ::std::string &vertexBufferFormat,
+        const ::std::vector<int> &indexBufferData
+    );
+
+    /* Create sphere primitive function.
+     * ARGUMENTS:
+     *   - sphere's radius:
+     *       float radius;
+     *   - sphere's position:
+     *       const math::vec3 &position;
+     *   - sphere's stacks:
+     *       int stacks;
+     *   - sphere's slices:
+     *       int slices;
+     * RETURNS:
+     *   (primitive *) - not-owning pointer to the created sphere primitive;
+     */
+    primitive *createSpherePrimitive(
+        float radius,
+        const math::vec3 &position,
+        int stacks = 20,
+        int slices = 20
+    );
+
+protected:
+    // Class constructor
+    explicit unit();
+
+    /* Unit initialization pure-virtual function.
+     * ARGUMENTS: None.
+     * RETURNS: None.
+     */
+    virtual void init() = 0;
+
+    /* Unit response pure-virtual function.
+     * ARGUMENTS: None.
+     * RETURNS: None.
+     */
+    virtual void response() = 0;
+
+    /* Render unit function.
+     * ARGUMENTS: None.
+     * RETURNS: None.
+     */
+    void render() const;
+
+    /* Clear unit function.
+     * ARGUMENTS: None.
+     * RETURNS: None.
+     */
+    void clear();
+
+    // Class virtual destructor
+    virtual ~unit() = default;
+};  // End of 'unit' class
+}  // namespace hse
+
+#endif  // UNIT_HPP
